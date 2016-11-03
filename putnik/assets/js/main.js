16057cb369a84c59f1f20183bf307c7307120882
Николай Федосеев <xneek@mail.ru>
2016-11-03 10:44:56 +0300

    init servisworker

--- putnik/assets/js/main.js
+++ putnik/assets/js/main.js
@@ -11,18 +11,6 @@ navRight	= document.getElementById('navRight');
 
 
 
-// ServiceWorker is a progressive technology. Ignore unsupported browsers
-if ('serviceWorker' in navigator) {
-  console.log('CLIENT: service worker registration in progress.');
-  navigator.serviceWorker.register('/putnik/assets/js/service-worker.js').then(function() {
-    console.log('CLIENT: service worker registration complete.');
-	}, function(a,b) {
-    console.log('CLIENT: service worker registration failure.',a,b);
-  });
-} else {
-  console.log('CLIENT: service worker is not supported.');
-}
-
 
 
 
