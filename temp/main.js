fc0df4da5d27c128bb23375977a626e16c605d44
Nikolay Fedoseev <xneek@mail.ru>
2018-02-26 17:26:39 +0300

    temps2

--- temp/main.js
+++ temp/main.js
@@ -28,4 +28,66 @@ function drawChart() {
     var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
 
     chart.draw(gdata, options);
+
+
+
+
+
+
+    var ddd = [
+        ['Дата', 'Температура устройство', 'Температура улица'],
+
+    ];
+    d2.forEach(function(x) {
+        ddd.push([new Date((x[0] * 1000)), x[1], x[3]])
+    })
+    var gdata = google.visualization.arrayToDataTable(ddd);
+    var options = {
+        title: 'Температура за весь поход',
+        curveType: 'function',
+        height: 600,
+        vAxis: {
+            title: 'Температура (°C)'
+        },
+        hAxis: {
+            title: 'Дата время'
+        },
+
+        legend: { position: 'bottom' }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('chart_div2'));
+
+    chart.draw(gdata, options);
+
+
+
+
+    var ddd = [
+        ['Дата', 'Давление'],
+
+    ];
+    d2.forEach(function(x) {
+        ddd.push([new Date((x[0] * 1000)), x[2]])
+    })
+    var gdata = google.visualization.arrayToDataTable(ddd);
+    var options = {
+        title: 'Атмосферное давление за поход',
+        curveType: 'function',
+        height: 600,
+        vAxis: {
+            title: 'Давление (гПа)'
+        },
+        hAxis: {
+            title: 'Дата время'
+        },
+
+        legend: { position: 'bottom' }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('chart_div3'));
+
+    chart.draw(gdata, options);
+
+
 }
