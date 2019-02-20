7246bfd01a67e69fa709eaac1ebecacdda16e2b4
pohodnik58 <pohodnik58@gmail.com>
2019-02-20 08:47:31 +0300

    Update about.js

--- app/pages/about.js
+++ app/pages/about.js
@@ -3,9 +3,10 @@ fetch('https://org.pohodnik58.ru/ajax/app/about.json')
     .then(function(response) {
         return response.json();
     }).then(function(res){
-        app.el.empty().append(
+        app.el.empty().append( crEl(
             crEl('h1','about'),
             crEl('h3', 'version: '+ res.version),
             crEl('p','last update '+ new Date(Date.parse(res.date)).toLocaleString())
+            )
         )
     })
